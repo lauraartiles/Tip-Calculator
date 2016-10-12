52c19ad8f2bb142967b6c16c397bd75b589547e0
lauraartiles <lauraartiles@utexas.edu>
2016-10-12 11:00:11 -0400

    Initial Commit

---
+++ TipCalculatorMakeSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculatorMakeSchool
+//
+//  Created by Laura Artiles on 10/12/16.
+//  Copyright © 2016 Laura Artiles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
